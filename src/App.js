73a995b221fdc27d46f4c18cea514d09ac69e605
Genis Bardales <genis96@yahoo.com>
2021-04-07 23:45:42 -0400

    header and footer

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Navbar from './components/Navbar/Navbar.js';
 // import AboutMe from './components/AboutMe'
 // import Portfolio from './components/Portfolio'
 // import Contact from './components/Contact'
-// import Wrapper from "./components/Wrapper";
+import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
 import './App.css';
@@ -15,14 +15,16 @@ function App() {
     <>
       <Router>
         <Navbar />
+        <Header>
         <Switch>
         <Redirect exact from="/" to="/Home" />
           {/* <Route path="/" exact component={AboutMe}/>
           <Route path="/" exact component={Portfolio}/>
           <Route path="/" exact component={Skills}/>
           <Route path="/" exact component={Contact}/> */}
-        </Switch>
-          <Footer />
+          </Switch>
+          </Header>
+        <Footer />
       </Router>
     </>
   );
