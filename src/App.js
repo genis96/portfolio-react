1e1c4c30e9e9f7924566160734b0df97cee19e70
Genis Bardales <genis96@yahoo.com>
2021-04-07 23:33:35 -0400

    added header and footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import Navbar from './components/Navbar/Navbar.js';
-import Home from './components/Home/Button'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+// import Home from './components/Home/Button'
+// import AboutMe from './components/AboutMe'
+// import Portfolio from './components/Portfolio'
+// import Contact from './components/Contact'
+// import Wrapper from "./components/Wrapper";
+import Footer from "./components/Footer";
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
 import './App.css';
 
 
@@ -11,8 +16,13 @@ function App() {
       <Router>
         <Navbar />
         <Switch>
-          <Route path="/" exact />
+        <Redirect exact from="/" to="/Home" />
+          {/* <Route path="/" exact component={AboutMe}/>
+          <Route path="/" exact component={Portfolio}/>
+          <Route path="/" exact component={Skills}/>
+          <Route path="/" exact component={Contact}/> */}
         </Switch>
+          <Footer />
       </Router>
     </>
   );
---
+++ src/components/Header/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./style.css";
+
+function Header(props) {
+  return <main className="header" {...props} />;
+}
+
+export default Header;
